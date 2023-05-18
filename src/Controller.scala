986f538173a2d5b89942fb0fe0779c852ac45032
Alex Lopes <alexkylebandeiralopes@gmail.com>
2023-05-18 14:21:38 +0100

    first commit

---
+++ src/Controller.scala
@@ -0,0 +1,12 @@
+import javafx.fxml.FXML
+import javafx.scene.control.{Button, TextField}
+
+class Controller {
+  @FXML
+  private var button1: Button = _
+  @FXML
+  private var textField1: TextField = _
+  def onButton1Clicked(): Unit = {
+    textField1.setText("Hello World")
+  }
+}
